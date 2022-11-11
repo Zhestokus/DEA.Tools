38f726bde1ac50f03534cc7b1fecae13c75e9786
Zhestokus <v.ezugbaia@gmail.com>
2022-11-11 16:19:37 +0400

    Add files via upload

---
+++ Source/DEA.Tools.Compression.Gzip/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using DEA.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEA.Tools.Compression.Gzip
+{
+    public static class Extensions
+    {
+        public static IDeaConnector UseGZipCompression(this IDeaConnector deaConnetor)
+        {
+            var instance = deaConnetor.UseCompression(() => new GZipCompressor());
+            return instance;
+        }
+
+        public static IDeaProcessor UseGZipCompression(this IDeaProcessor deaProcessor)
+        {
+            var instance = deaProcessor.UseCompression(() => new GZipCompressor());
+            return instance;
+        }
+    }
+}
