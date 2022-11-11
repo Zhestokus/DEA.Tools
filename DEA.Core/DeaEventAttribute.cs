b8742f293fcb333a2918e5d74138235be7ae54e9
Zhestokus <v.ezugbaia@gmail.com>
2022-11-11 15:52:17 +0400

    Add files via upload

---
+++ DEA.Core/DeaEventAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEA.Core
+{
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class DeaEventAttribute : Attribute
+    {
+        public DeaEventAttribute()
+        {
+        }
+
+        public DeaEventAttribute(string eventName)
+        {
+            EventName = eventName;
+        }
+
+        public string EventName { get; private set; }
+    }
+}
---
+++ DEA.Core/ThreadingMode.cs
@@ -0,0 +1,10 @@
+﻿namespace DEA.Core
+{
+    public enum ThreadingMode
+    {
+        SingleThread = 0,
+        MultiThread = 1,
+        ThreadPool = 2,
+        TaskHandled = 3
+    }
+}
